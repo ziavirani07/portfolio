8741fbe5b4f27a09a7273dd06a20bede8154daac
isfelaco <isfelaco@gmail.com>
2025-04-27 17:06:40 -0400

    some basic instructions

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography } from "@mui/material";
+
 export function Header() {
 	return (
 		<Box
--- src/theme.ts
+++ src/theme.ts
@@ -3,47 +3,28 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
 	palette: {
 		primary: {
-			main: "#ffffff",
+			main: "#ffffff", // white
 		},
 		secondary: {
-			main: "#9dbf9e",
-			light: "#d0d6b5",
+			main: "#9dbf9e", // darker green
+			light: "#d0d6b5", // lighter green
 		},
 		info: {
-			main: "#987284",
+			main: "#987284", // purple
 		},
 		background: {
-			paper: "#ffffff",
-			default: "#d0d6b5",
+			paper: "#ffffff", // white
+			default: "#d0d6b5", // lighter green
 		},
 	},
 	typography: {
 		fontFamily: '"Oranienbaum", "Helvetica", "Arial", sans-serif',
-		h1: {
-			fontFamily: '"Oranienbaum", "Helvetica", "Arial", sans-serif',
-			fontWeight: 700,
-		},
-		h2: {
-			fontFamily: '"Oranienbaum", "Helvetica", "Arial", sans-serif',
-			fontWeight: 700,
-		},
-		h3: {
-			fontFamily: '"Oranienbaum", "Helvetica", "Arial", sans-serif',
-			fontWeight: 400, // Use bold weight for h1
-		},
-		h4: {
-			fontFamily: '"Oranienbaum", "Helvetica", "Arial", sans-serif',
-			fontWeight: 400, // Use bold weight for h1
-		},
-		body1: {
-			fontFamily: '"Oranienbaum", "Helvetica", "Arial", sans-serif',
-			fontWeight: 400,
-		},
-		subtitle1: {
-			fontFamily: '"Oranienbaum", "Helvetica", "Arial", sans-serif',
-			fontWeight: 400,
-			color: "#ffffff",
-		},
+		h1: { fontWeight: 700 },
+		h2: { fontWeight: 700 },
+		h3: { fontWeight: 400 },
+		h4: { fontWeight: 400 },
+		body1: { fontWeight: 400 },
+		subtitle1: { fontWeight: 700, color: "#ffffff" },
 	},
 	spacing: 4,
 });
